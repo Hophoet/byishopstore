f54e225b775aa5bedbec743e5fdb7ac6b16cea73
hophoet <hophoet@gmail.com>
2021-03-01 19:59:31 +0000

    (htlm) - add the index page html

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mockup from './mockup.png';
+import ps from './ps.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <div className="main_container">
+            <div className="labels">
+                <div>
+                  
+                    <p><h1>Byi</h1> shop</p>
+                
+                   
+                </div>
+               <div>
+                    <h4> Lorem ipsum dolor sit amet dolor sit amet consectetur, </h4>
+                    <a href="">
+                      <img className='playstore' src={ps} alt=""/>
+                    </a>
+               </div>
+               <div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus tempore asperiores magni rerum assumenda voluptatibus voluptates odio, molestias harum corrupti officia quis!</p>
+               </div>
+            </div>
+            <div className="mockup">
+                <img src={mockup} alt=""/>
+            </div>
+        </div>
     </div>
   );
 }
