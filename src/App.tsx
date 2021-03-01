355a20aa161df7f192b799754967c2052eb06bd0
hophoet <hophoet@gmail.com>
2021-03-01 23:33:20 +0000

    (components) - update email subscription component textinput with button

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,25 @@ function App() {
         <div className="main_container">
             <div className="labels">
                 <div>
-                  
                     <p><h1>Byi</h1> shop</p>
-                
-                   
                 </div>
                <div>
-                    <h4> Lorem ipsum dolor sit amet dolor sit amet consectetur, </h4>
+                    <h2> Lorem ipsum dolor sit amet dolor sit amet consectetur, </h2>
                     <a href="">
                       <img className='playstore' src={ps} alt=""/>
                     </a>
+                  
                </div>
+               <div className='main__form'>
+                  <div className='main__form_title_container'> 
+                      <h3>Inscrivez-vous pour plus d'informations</h3>
+                  </div>
+                  <div>
+                    <input type="text" placeholder='Enter votre email'/>
+                    <button>Souscrire</button>
+                  </div>  
+                 
+                </div>
                <div>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus tempore asperiores magni rerum assumenda voluptatibus voluptates odio, molestias harum corrupti officia quis!</p>
                </div>
