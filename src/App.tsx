cc8a39dfa7103e717c86bf90cc54c027b6ef5113
hophoet <hophoet@gmail.com>
2021-03-03 13:19:43 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React, {MouseEvent, ReactHTMLElement, ChangeEvent} from 'react';
 
 import './App.css';
 import mockup from './mockup.png';
+import gif from './assets/l.gif'
 import ps from './ps.png'
-import {subscribe} from './api/functions'
+import lg from './assets/lg.png'
+import {subscribe, playStoreURL} from './api/functions'
 
 interface Props {
   defaultEmail?:string
@@ -16,7 +18,7 @@ export default class App extends React.Component<Props, State>{
   constructor(props: Props){
     super(props);
     this.state = {email:this.props.defaultEmail}
-   
+    
   }
 
   handleSubmit = (event:MouseEvent) => {
@@ -42,17 +44,19 @@ export default class App extends React.Component<Props, State>{
      
         <div className="main_container">
             <div className="labels">
-                <div>
-                    <p><h1>Byi</h1> shop</p>
+                <div className='logo_container'>
+                    <img className='logo' src={lg} alt=""/>
+                    <h1>Byi</h1>
                 </div>
                <div>
-                    <h2> Lorem ipsum dolor sit amet dolor sit amet consectetur, </h2>
-                    <a href="">
-                      <img className='playstore' src={ps} alt=""/>
+                    <h2>ByiShop qui permet aux Etudiants a faire du shoping entre eux</h2>
+                    <a href={playStoreURL} target='_blank'>
+                      <img className='playstore' src={ps} alt="" />
                     </a>
                   
                </div>
                <div className='main__form'>
+               
                   <div className='main__form_title_container'> 
                       <h3>Inscrivez-vous pour plus d'informations</h3>
                   </div>
@@ -63,7 +67,16 @@ export default class App extends React.Component<Props, State>{
                  
                 </div>
                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus tempore asperiores magni rerum assumenda voluptatibus voluptates odio, molestias harum corrupti officia quis!</p>
+
+                    <ul>
+                      <li>ByiShop est une application géniale et facile a utilisé qui permet a un Etudiant(e)s de mettre en disponibilité des produits à d'autres Etudiants afin que ces derniers puisse commandés et livrés par l'Etudiants en question.</li>
+                      <li>L'application permet a l'etudiant(e)s d'ajouter ces propres articles, vetements, matérielles ou ses propre création qu'il souhaiterai ventre au autres etudiant(e)s</li>
+                      <li>
+L'application permet au etudiant d'avoir un vue d'ensemble sur l'etat de leur produits disponibles et les commandes relatives a ces produits.
+
+</li>
+                      <li>L'application intègre un systeme de livraison trés facile a utilisé qui permet aux etudiants de valider la livraison de leurs produits par un simple geste qui consiste a scanner un QR code de la commande afin de valider la livraison en toute sécurité.</li>
+                    </ul>
                </div>
             </div>
             <div className="mockup">
