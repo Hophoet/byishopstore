2976b2232666290a2127ba60be4de94aaf43f9a2
hophoet <hophoet@gmail.com>
2021-03-26 09:00:17 +0000

    (styles) - remove import @coreui styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "@coreui/coreui/scss/coreui.scss";
 ReactDOM.render(
   <React.StrictMode>
     <App />
