6d34deb72ee5a677d3aff75bd4526a8febadc45b
hophoet <hophoet@gmail.com>
2021-03-26 08:54:51 +0000

    (components) -  set app icon, add instagram external link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from "react";
+
 const Footer = ({logo}) => {
   return (
     <div id="contact" className="flex flex-row flex-wrap items-center justify-around p-10">
-   
+        <img src={logo}/>
+        <div>
+          <a href='https://www.instagram.com/hophoet' target="_blank">Nous suivre</a>
+        </div>
     </div>
   );
 };
